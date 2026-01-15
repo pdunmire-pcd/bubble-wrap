9e705fb7c3419beb698c5ec7fdf5dd7e3f386371
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-01-15 04:18:45 +0000

    push a button backdrop turns red

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
+
 
 
 int main() {
     bn::core::init();
     bn::backdrop::set_color(bn::color(31,18,22));
-    while(true) {
-    bn::core::update();
-    }   
 
+    
+    while(true) {
+        
+        if (bn::keypad::a_pressed()) {
+            bn::backdrop::set_color(bn::color(27,2,7));
+            }
+
+        bn::core::update();
+    }   
+    
 }
